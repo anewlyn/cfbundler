87793a4fa1255df28d2d571b0e7c45c1ec8535fb
travis-cox2 <tcox2@apple.com>
2024-05-20 15:18:36 -0700

    Found a regression

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,13 +1,10 @@
 'use client'
 
-import classNames from "classnames";
 import Image from "next/image";
 import { useState } from "react";
-import { useLoopContext } from "@/app/contexts/LoopProvider";
 import AddToButton from "./AddToButton";
 
 const ProductCard = ({ product }: any) => {
-  const { isMobile } = useLoopContext();
   const [qty, setQty] = useState(1);
 
   const { imageURL, title, price, outOfStock } = product.variants[0];
@@ -19,8 +16,8 @@ const ProductCard = ({ product }: any) => {
   }
 
   return (
-    <div className={classNames('product-card')}>
-      <div className={classNames("product-image", { 'info-button-mobile': isMobile })}>
+    <div className='product-card'>
+      <div className="product-image">
         <Image src={imageURL} alt={title} width={309} height={309} />
         <div className={"info-screen"}>
           <button onClick={handleOpenInfoModal} className="info-button">MORE INFO</button>
