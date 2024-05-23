c7039291410d1b59ecb4cc75d347038cdffff3fd
travis-cox2 <tcox2@apple.com>
2024-05-23 12:54:16 -0700

    Head bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet" />
-      </Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet" />
       <body >{children}</body>
     </html>
   );
