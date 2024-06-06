94c2de2734a279e52391b9be6e2869f36b7bf031
travis-cox2 <tcox2@apple.com>
2024-06-06 15:01:07 -0700

    rework progress bar

--- src/components/BenefitTierProgressBar.tsx
+++ src/components/BenefitTierProgressBar.tsx
@@ -2,7 +2,6 @@ import classNames from 'classnames';
 
 interface BenefitTier {
   value: number;
-  label: string;
   subtitle: string;
 }
 
@@ -12,8 +11,6 @@ interface BenefitTierProgressBarProps {
 }
 
 const BenefitTierProgressBar = ({ currentValue, tiers }: BenefitTierProgressBarProps) => {
-  const maxTierValue = Math.max(...tiers.map((tier) => tier.value));
-  const overlayWidth = Math.min((currentValue / maxTierValue) * 100, 100);
   return (
     // grid
     <div className="progress-bar">
@@ -21,21 +18,37 @@ const BenefitTierProgressBar = ({ currentValue, tiers }: BenefitTierProgressBarP
       <div className={classNames(`grid-cols-${tiers.length}`)}>
         {tiers.map((tier, index) => {
           const isValueMet = tier.value <= currentValue;
+          const previousTierValue = tiers[index - 1]?.value || 0;
+          const tierRange = tier.value - previousTierValue;
+          const tierOverlayWidth = Math.min(
+            ((currentValue - previousTierValue) / tierRange) * 100,
+            100,
+          );
+          const showOverlay = tierOverlayWidth > 0;
 
           return (
             <div className={classNames('tier-marker')} key={index}>
               {/* grid rows */}
               <div className="tier-line row-1" />
-              <div>{tier.label}</div>
+              <div>
+                {Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
+                  maximumFractionDigits: 0,
+                }).format(tier.value)}
+              </div>
               <div className="tier-subtitle">
                 <i className={classNames('material-icons', !isValueMet && 'no-display')}>check</i>
                 {tier.subtitle}
               </div>
+              <div
+                className="progress-bar-overlay"
+                style={{ width: `${showOverlay ? tierOverlayWidth : 0}%` }}
+              />
             </div>
           );
         })}
       </div>
-      <div className="progress-bar-overlay" style={{ width: `${overlayWidth}%` }} />
     </div>
   );
 };
--- src/contexts/LoopProvider.tsx
+++ src/contexts/LoopProvider.tsx
@@ -7,7 +7,7 @@ import { getCartValue, setProductsForRender } from '@/helpers/cartHelpers';
 
 export type LoopContextType = {
   addProductVariant: ({ shopifyId, quantity }: variantType) => void;
-  benefitTiers: { label: string; subtitle: string; footerMessage: string; value: number }[];
+  benefitTiers: { subtitle: string; footerMessage: string; value: number }[];
   currentOrderValue: number;
   deliverCadence: string[];
   mockOrder: mockOrderTypes;
@@ -91,27 +91,26 @@ const LoopProvider = ({
   //   const getCartValue = getCartValue()
   // }
 
-  // @todo Create update functions to update the data
   const benefitTiers = [
     {
-      label: '$50',
       subtitle: 'Min. Order',
       footerMessage: 'Subscriptions require a $50 minimum order.',
       value: 50,
     },
     {
-      label: '$100',
       footerMessage: 'Yay! You have free shipping.',
       subtitle: 'Free Shipping',
       value: 100,
     },
     {
-      label: '$150',
       footerMessage: 'Yay! You have free shipping and a 10% discount.',
       subtitle: '10% off',
       value: 150,
     },
-  ];
+  ].map((tier, index) => ({
+    ...tier,
+    value: bundleData.discounts[index].minCartQuantity,
+  }));
 
   const deliverCadence = ['2 WEEKS', '4 WEEKS', '6 WEEKS', '8 WEEKS'];
 
