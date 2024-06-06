1e5c79d9652d3ce6c51db38c38491f8da0e86374
travis-cox2 <tcox2@apple.com>
2024-06-06 09:20:36 -0700

    lint fix

--- src/app/bundler/layout.tsx
+++ src/app/bundler/layout.tsx
@@ -5,9 +5,7 @@ const BundlerLayout = async ({ children }: { children: React.ReactNode }) => {
   const bundleData = await getBundle();
 
   return (
-    <LoopProvider
-      bundleData={bundleData.data}
-    >
+    <LoopProvider bundleData={bundleData.data}>
       <section>{children}</section>
     </LoopProvider>
   );
