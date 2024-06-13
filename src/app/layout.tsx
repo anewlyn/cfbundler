b988823a18ae4b440125c5b866dede7020c9f3ea
travis-cox2 <tcox2@apple.com>
2024-06-13 12:33:26 -0700

    set initial scale as own prop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.className} antialiased`}>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="viewport" content="width=device-width" initial-scale="1" />
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
       <body>{children}</body>
     </html>
