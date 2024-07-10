76b4e26400deae5e9e08c0569032c1c31fc55ae5
Ryan V <47340962+denvermullets@users.noreply.github.com>
2024-07-10 16:45:10 -0400

    fix(app): move app to load on root instead of /bundler (#23)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,65 @@
+import { Metadata } from 'next';
 import '../styles/globals.scss';
 import { inter } from '@/app/ui/fonts';
+import LoopProvider from '@/contexts/LoopProvider';
+import { getBundle } from './api/loop/getBundle';
+import getProducts from './api/shopify/getProducts';
 
-export default function RootLayout({
+export const metadata: Metadata = {
+  title: 'Cycling Frog',
+  description:
+    "Cycling Frog isn't just a brand. It's a statement. It's the belief that cannabis consumption should be normalized. Cannabis should be affordable. Cannabis should be accessible. Cannabis is fun. Cannabis should be enjoyed with the same ease as your favorite sparkling water or beer, and we at Cycling Frog are here to make that happen.",
+  generator: 'Next.js',
+  applicationName: 'Next.js',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript'],
+  authors: [
+    { name: 'Travis Cox' },
+    { name: 'Ryan Vaznis' },
+    { name: 'Ryan Canfield' },
+    { name: 'Eva Wood' },
+    { name: 'Kim Franklin' },
+    { name: 'Ethan Grebmeier' },
+    { name: 'Sam Reed' },
+  ],
+  creator: 'Assemble Inc.',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: 'Cycling Frog',
+    description:
+      "Cycling Frog isn't just a brand. It's a statement. It's the belief that cannabis consumption should be normalized. Cannabis should be affordable. Cannabis should be accessible. Cannabis is fun. Cannabis should be enjoyed with the same ease as your favorite sparkling water or beer, and we at Cycling Frog are here to make that happen.",
+    url: 'https://cyclingfrog.com/',
+    siteName: 'Cycling Frog Bundler',
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+};
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const bundleData = await getBundle();
+  const shopifyProducts = await getProducts();
+
   return (
     <html lang="en" className={`${inter.className} antialiased`}>
       <meta name="viewport" content="width=device-width" initial-scale="1" />
@@ -15,11 +69,15 @@ export default function RootLayout({
         // @ts-ignore - NextJS requires this unsupported attribute
         precedence="default"
       />
-      <body>{children}</body>
-      <script
-        async
-        src={`//loox.io/widget/loox.js?shop=${process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN}`}
-      />
+      <body>
+        <LoopProvider bundleData={bundleData.data} shopifyProducts={shopifyProducts}>
+          <section>{children}</section>
+        </LoopProvider>
+        <script
+          async
+          src={`//loox.io/widget/loox.js?shop=${process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN}`}
+        />
+      </body>
     </html>
   );
 }
