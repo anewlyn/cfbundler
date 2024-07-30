927bdf8ff3929f890e845ae32ad83ea8f9ca82b0
denvermullets <devmullets@gmail.com>
2024-07-30 12:41:14 -0400

    fix(config): serve assets normally if staging/local

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,25 @@
 
 const isProd = process.env.NODE_ENV === 'production';
 
+const prodDomains = ['cdn.shopify.com', 'bundler.cyclingfrog.com'];
+const nonProdDomains = ['cdn.shopify.com'];
+
 const nextConfig = {
   sassOptions: {
     includePaths: ['./src/styles/'],
   },
   images: {
-    domains: ['cdn.shopify.com', 'bundler.cyclingfrog.com'],
-    path: isProd ? 'https://bundler.cyclingfrog.com/_next/image' : undefined,
+    domains: isProd ? prodDomains : nonProdDomains,
+    ...(isProd && {
+      path: 'https://bundler.cyclingfrog.com/_next/image',
+    }),
   },
-  assetPrefix: isProd ? 'https://bundler.cyclingfrog.com' : '',
-  publicRuntimeConfig: {
-    assetPrefix: isProd ? 'https://bundler.cyclingfrog.com' : '',
-  },
+  ...(isProd && {
+    assetPrefix: 'https://bundler.cyclingfrog.com',
+    publicRuntimeConfig: {
+      assetPrefix: 'https://bundler.cyclingfrog.com',
+    },
+  }),
 };
 
 export default nextConfig;
