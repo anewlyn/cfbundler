27516e6d8f7e619000bf3137e715ac633b4bf1c5
Ethan Grebmeier <ethan@assembleinc.com>
2024-09-17 14:27:52 -0700

    fix: remove try catch for dynamic calls

--- src/app/api/loop/createTransaction.ts
+++ src/app/api/loop/createTransaction.ts
@@ -9,25 +9,18 @@ const createTransaction = async (cart: CartType, id: string) => {
       'content-type': 'application/json',
     },
   };
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_LOOP_API_URL}${process.env.NEXT_PUBLIC_LOOP_API_VERSION}/bundle/${id}/transaction`,
+    {
+      ...options,
+      body: JSON.stringify(cart),
+    },
+  );
 
-  try {
-    // @todo this returns a transaction id that needs to be passed to shopify cart
-    // TODO: remove / from public-loop-api-version env variable
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_LOOP_API_URL}${process.env.NEXT_PUBLIC_LOOP_API_VERSION}/bundle/${id}/transaction`,
-      {
-        ...options,
-        body: JSON.stringify(cart),
-      },
-    );
+  const transactionResponse = await response.json();
+  const txnId = transactionResponse.data.txnId;
 
-    const transactionResponse = await response.json();
-    const txnId = transactionResponse.data.txnId;
-
-    return txnId;
-  } catch (err) {
-    console.error(err);
-  }
+  return txnId;
 };
 
 export default createTransaction;
