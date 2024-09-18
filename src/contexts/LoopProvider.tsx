2246bbc5edc02ced0f0bd3b704f55f3cc550fec9
Ethan Grebmeier <ethan@assembleinc.com>
2024-09-18 16:59:37 -0700

    chore: check revert

--- src/contexts/LoopProvider.tsx
+++ src/contexts/LoopProvider.tsx
@@ -52,9 +52,7 @@ const setBenefitTierContents = (discounts: DiscountTypes[], tiers: BenefitTierTy
   return tiers.map((tier, index) => {
     return {
       ...tier,
-      footerMessage: tier.footerMessage
-        .replace('$$', discounts[index].value.toString())
-        .replace('^^', discounts[index + 1] ? discounts[index + 1].value.toString() : ''),
+      footerMessage: tier.footerMessage.replace('$$', discounts[index].value.toString()),
       subtitle: tier.subtitle.replace('$$', discounts[index].value.toString()),
       value: discounts[index].minCartQuantity,
     };
