9bc31f15447107bf2f1de31edffcaaccdc7cc60b
juanlncf <jmoncada@lazarusnaturals.com>
2025-12-04 15:40:45 -0800

    just try

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -121,7 +121,7 @@ const StickyFooter = ({ customProducts }) => {
     console.log('arr', arr)
 
     console.log('carouselRef?.current?.children.length', carouselRef?.current?.children.length)
-    if(emblaApi) emblaApi?.scrollTo(carouselRef?.current?.children.length)
+    if(emblaApi) emblaApi?.scrollTo(carouselRef?.current?.children.length + 1)
 
     return arr;
   }, [cart.productVariants, products])
