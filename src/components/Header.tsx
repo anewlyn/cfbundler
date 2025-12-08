8b9396d2db9b2b65dcb2a611674e204101366119
juanlncf <jmoncada@lazarusnaturals.com>
2025-12-08 11:22:26 -0800

    header;

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,7 @@ import { kiro_bold_400 } from '@/app/ui/fonts';
 import { useLoopContext } from '@/contexts/LoopProvider';
 import BenefitTierProgressBar from './BenefitTierProgressBar';
 import DeliveryFrequencyDropdown from './DeliveryFrequencyDropdown';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -23,52 +24,27 @@ const Header = () => {
     window.addEventListener('scroll', onScroll);
     onScroll();
     return () => window.removeEventListener('scroll', onScroll);
-  }, []);
-
-  const ScheduleButton = (className: string) => (
-    <DeliveryFrequencyDropdown className={className} />
-  );
+  }, [])
 
   return (
-    <header
-      className="header"
-      style={
-        { '--header-mobile-top': isScrolled ? `-${headerTitleHeight}px` : '0' } as React.CSSProperties
-      }
-    >
-      <div className="header-logo">
-        <a href="https://cyclingfrog.com">
-          <Image
-            alt="Logo"
-            className="logo"
-            height={93}
-            src="https://bundler.cyclingfrog.com/assets/cycling-frog-logo.png"
-            width={170}
+    <Navbar>
+      <Container>
+        <Nav>
+          <Nav.Link href="https://cyclingfrog.com">
+            <- Back
+          </Nav.Link>
+        </Nav>
+        <Navbar.Brand>
+          <Image 
+            style={{ height: '40px' }}
+            alt='Cycling Frog Logo'
+            src={'https://cyclingfrog.com/cdn/shop/files/CyclingFrog_CMYK_Logo-LeftAlignedStacked-Regulated-Black_300x.png?v=1710802940'}
+            width={300}
+            height={150}
           />
-        </a>
-      </div>
-
-      <div ref={headerTitleRef} className={classNames('header-title', kiro_bold_400.className)}>
-        <h1 className="uppercase">My Subscription</h1>
-        {ScheduleButton('header-button')}
-      </div>
-
-      <div className="header-progress-bar">
-        <BenefitTierProgressBar currentValue={currentOrderValue} tiers={benefitTiers} />
-      </div>
-
-      <div className="cf-bundle-progress" style={{ position: 'fixed'}}>
-        <div className="cf-bundle-progress-discount cf-text-heading cf-uppercase">
-          <span className="cf-bundle-progress-discount-percentage">10%</span>
-          <span>Off</span>
-        </div>
-        <svg className="cf-bundle-progress-bar" width="64px" height="64px" viewBox="0 0 72 72" style={{ transform: 'rotate(-90deg)'}}>
-          <circle r="32" cx="36" cy="36" fill="transparent" stroke="#ffb3ab" stroke-width="8"></circle>
-          <circle r="32" cx="36" cy="36" fill="transparent" stroke="#000000" stroke-width="8" stroke-linecap="round" stroke-dashoffset="132px" stroke-dasharray="200.96px"></circle>
-        </svg>
-      </div>
-      
-    </header>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
   );
 };
 
