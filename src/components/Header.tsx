f67a6c216df48f14531f524a901466030c7afdaa
juanlncf <jmoncada@lazarusnaturals.com>
2025-12-08 11:25:13 -0800

    navbar;

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,17 @@
 'use client';
 
-import classNames from 'classnames';
 import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
-import { kiro_bold_400 } from '@/app/ui/fonts';
-import { useLoopContext } from '@/contexts/LoopProvider';
-import BenefitTierProgressBar from './BenefitTierProgressBar';
-import DeliveryFrequencyDropdown from './DeliveryFrequencyDropdown';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
-  const [headerTitleHeight, setHeaderTitleHeight] = useState(0);
-  const { benefitTiers, currentOrderValue } = useLoopContext();
-  const headerTitleRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const stickyThreshold = 100;
     const onScroll = () => {
       setIsScrolled(window.scrollY > stickyThreshold);
-      setHeaderTitleHeight(headerTitleRef.current?.offsetHeight || 0);
-    };
+    }
     window.addEventListener('scroll', onScroll);
     onScroll();
     return () => window.removeEventListener('scroll', onScroll);
