0c9de514981186d71a501a253d4d280f1c4f3f7e
juanlncf <jmoncada@lazarusnaturals.com>
2025-12-08 11:38:50 -0800

    nav;

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,23 +18,24 @@ const Header = () => {
   }, [])
 
   return (<>
-    <Navbar>
+    <Navbar style={{ backgroundColor: '#FFB3AB' }}>
       <Container>
         <Nav>
-          <Nav.Link href="https://cyclingfrog.com">
+          <Nav.Link href="https://cyclingfrog.com" className='d-flex p-2 justify-content-center align-items-center'>
             <i className='material-icons'>arrow_back</i> 
             Back
           </Nav.Link>
         </Nav>
         <Navbar.Brand>
           <Image 
-            style={{ height: '40px' }}
+            style={{ width: 'auto', height: '40px' }}
             alt='Cycling Frog Logo'
             src={'https://cyclingfrog.com/cdn/shop/files/CyclingFrog_CMYK_Logo-LeftAlignedStacked-Regulated-Black_300x.png?v=1710802940'}
             width={300}
             height={150}
           />
         </Navbar.Brand>
+        <span></span>
       </Container>
     </Navbar>
   </>)
