77827e33cd33e23e6d4ff8b9b43ece2e62080348
juanlncf <jmoncada@lazarusnaturals.com>
2025-12-09 13:52:12 -0800

    move btns;

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -224,14 +224,14 @@ const StickyFooter = ({ customProducts }) => {
         </div>
 
         {!items.length && <p className="cf-footer-message">
-          { items.length && <>
-            <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-            <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
-          </> }
           { getFooterMessage() }
         </p>}
         
         <div className="cf-footer-actions">
+          { items.length && <>
+            <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+            <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+          </>}
           <Frequency />
           <button
             onClick={handlePostTransaction}
