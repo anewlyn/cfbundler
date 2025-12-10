143445fa73f25ed1092e68f027edbf7546dba05a
juanlncf <jmoncada@lazarusnaturals.com>
2025-12-10 11:17:50 -0800

    oops old buttons;

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -228,7 +228,6 @@ const StickyFooter = ({ customProducts }) => {
         </p>}
         
         <div className="cf-footer-actions">
-          { !!items.length && <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} /> }
           <Frequency />
           <button
             onClick={handlePostTransaction}
@@ -238,7 +237,6 @@ const StickyFooter = ({ customProducts }) => {
             { submittingCart ? `Adding to cart...` : `Add to Cart ` }
             { renderProductPrice() }
           </button>
-          { !!items.length && <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} /> }
         </div>
       </div>
     </div>
