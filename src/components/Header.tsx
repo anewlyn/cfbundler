f8d451fb9d4a7b7a10617f768fde4f0fe72f9026
juanlncf <jmoncada@lazarusnaturals.com>
2025-12-10 11:12:57 -0800

    ok;

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { kiro_bold_400 } from '@/app/ui/fonts'
+import { kiro_bold_400, kiro_extra_bold_700 } from '@/app/ui/fonts'
 import { useLoopContext, LoopContextType } from '@/contexts/LoopProvider'
 import Image from 'next/image'
 import { useEffect, useMemo, useRef, useState } from 'react'
@@ -69,7 +69,7 @@ const Header = () => {
         <span style={{ width: '5ch'}}></span>
         {benefitTiers &&
           <div className="cf-bundle-progress" style={{ position: 'fixed'}}>
-            <div className={`${kiro_bold_400.className} cf-bundle-progress-discount cf-text-heading cf-uppercase`} style={{ color: currentDiscount?.value ? '#000' : '#888' }}>
+            <div className={`${kiro_extra_bold_700.className} cf-bundle-progress-discount ${currentDiscount?.value ? 'active' : ''}`} style={{ color: currentDiscount?.value ? '#000' : '#888' }}>
               <span className="cf-bundle-progress-discount-percentage">
                 {currentOrderValue < benefitTiers[0].value
                   ? '10-20%'
