2a60f2e2a36d2fab24ff5ab374a78392312a7fb8
juanlncf <jmoncada@lazarusnaturals.com>
2025-12-16 12:28:58 -0800

    btn;

--- src/components/CounterBlock.tsx
+++ src/components/CounterBlock.tsx
@@ -25,7 +25,7 @@ const CounterBlock = ({ orderQty, setQty, maxQty }: CounterBlockProps) => {
   };
 
   return (
-    <div className="subscription-button">
+    <div className="btn btn-black subscription-button">
       <button onClick={decrement} className="icon-button">
         { orderQty === 1
           ? <svg width="24px" viewBox="0 0 16 16"><path fill="currentColor" d="M5.2,13.6c-.3,0-.6-.1-.8-.4-.2-.2-.4-.5-.4-.8v-8h-.2c-.2,0-.3,0-.4-.2-.1-.1-.2-.3-.2-.4s0-.3.2-.4c.1-.1.3-.2.4-.2h2.6v-.2c0-.2,0-.3.2-.4.1-.1.3-.2.4-.2h2c.2,0,.3,0,.4.2s.2.3.2.4v.2h2.6c.2,0,.3,0,.4.2.1.1.2.3.2.4s0,.3-.2.4c-.1.1-.3.2-.4.2h-.2v8c0,.3-.1.6-.4.9-.2.2-.5.4-.8.4h-5.6ZM10.8,4.4h-5.6v8h5.6v-8ZM7,11.2c.2,0,.3,0,.4-.2.1-.1.2-.3.2-.4v-4.4c0-.2,0-.3-.2-.4-.1-.1-.3-.2-.4-.2s-.3,0-.4.2c-.1.1-.2.3-.2.4v4.4c0,.2,0,.3.2.4.1.1.3.2.4.2ZM9,11.2c.2,0,.3,0,.4-.2.1-.1.2-.3.2-.4v-4.4c0-.2,0-.3-.2-.4-.1-.1-.3-.2-.4-.2s-.3,0-.4.2c-.1.1-.2.3-.2.4v4.4c0,.2,0,.3.2.4.1.1.3.2.4.2Z"/></svg> 
