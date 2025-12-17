3daf1c2845078f3291be030c70b0aeaed7f2674c
juanlncf <jmoncada@lazarusnaturals.com>
2025-12-17 12:55:39 -0800

    Revert "modalhydrogen;"
    This reverts commit ef030e28180d73914c5c633bdc8425b959d54150.

---
+++ src/components/RichText.js
@@ -0,0 +1,43 @@
+function Inline({item}) {
+    return item.bold ? <b>{item.value}</b> : item.italic ? <i>{item.value}</i> : {item.value}
+}
+
+function Paragraph({child}) {
+    return (<p>
+        {child.children?.map(grandchild => (<>
+            {grandchild.children?.map(item => (<>
+                    {item.type === 'text' && <Inline item={item} />}
+                </>))
+            }
+        </>))}
+    </p>)
+}
+
+function List({child}) {
+    return (<ul>
+        {child.children?.map((grandchild, i) => (
+            <li key={i}>
+                {grandchild.children?.map(item => 
+                    item.bold 
+                    ? <b>{item.value}</b>
+                    : item.italic
+                        ? <i>{item.value}</i>
+                        : <span>{item.value}</span>
+                )}
+            </li>
+        ))}
+    </ul>)
+}
+
+export default function RichText({text}) {
+    return (<>
+        {text.children.map((item, i) => (<>
+            {item.type === 'paragraph' 
+                ? <Paragraph key={i} child={item} />
+                : item.type === 'list'
+                    ? <List key={i} child={item} />
+                    : <Inline item={item} />
+            }
+        </>))}
+    </>)
+}
