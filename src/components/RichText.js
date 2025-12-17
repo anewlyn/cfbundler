b52ded8001bb17697c54d2abcefa8647acdbddf2
juanlncf <jmoncada@lazarusnaturals.com>
2025-12-17 09:19:02 -0800

    modal;

--- src/components/RichText.js
+++ src/components/RichText.js
@@ -1,24 +1,26 @@
 export default function RichText(value) {
-    switch (value.type) {
-        case 'paragraph':
-            return <p key={i}>{ value.value }</p>
-            break
-        case 'list':
-            return (<ul>
-                {value.children?.map((child, i) => (
-                    <li>
-                        {child.children?.map(item => 
-                            item.bold 
-                            ? <b>{item.value}</b>
-                            : item.italic
-                                ? <i>{item.value}</i>
-                                : <span>{item.value}</span>
-                        )}
-                    </li>
-                ))}
-            </ul>)
-            break
-        default:
-            break
-    }
+    value.children.map(child => {
+        switch (child.type) {
+            case 'paragraph':
+                return <p key={i}>{ child.value }</p>
+                break
+            case 'list':
+                return (<ul>
+                    {child.children?.map((grandchild, i) => (
+                        <li>
+                            {grandchild.children?.map(item => 
+                                item.bold 
+                                ? <b>{item.value}</b>
+                                : item.italic
+                                    ? <i>{item.value}</i>
+                                    : <span>{item.value}</span>
+                            )}
+                        </li>
+                    ))}
+                </ul>)
+                break
+            default:
+                break
+        }
+    })
 }
