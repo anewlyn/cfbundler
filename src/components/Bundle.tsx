4f9b85ab09bd82ac5d6e35846711f1a3ee8282e4
juanlncf <jmoncada@lazarusnaturals.com>
2026-01-08 14:51:13 -0800

    spans;

--- src/components/Bundle.tsx
+++ src/components/Bundle.tsx
@@ -57,14 +57,14 @@ export const Bundle = (customProductData) => {
             >
               <div className="cf-tiers-tier-progress-text">
                 {currentOrderValue >= tier.value
-                  ? <><i className="material-icons check-icon">check</i> {tier.subtitle}</>
+                  ? <span><i className="material-icons check-icon">check</i> {tier.subtitle}</span>
                   : <>{tier.subtitle.split('Get')[1]} <span>at {currencyFormatter(tier.value, 'USD', 0)}</span></>
                 }
               </div>
               <div className="cf-tiers-tier-progress-fill"></div>
               <div className="cf-tiers-tier-default-text">
                 {currentOrderValue >= tier.value
-                  ? <><i className="material-icons check-icon">check</i> {tier.subtitle}</>
+                  ? <span><i className="material-icons check-icon">check</i> {tier.subtitle}</span>
                   : <>{tier.subtitle.split('Get')[1]} <span>at {currencyFormatter(tier.value, 'USD', 0)}</span></>
                 }
               </div>
