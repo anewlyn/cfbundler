cf649731c33dd30a0cc32528cd4c019763123992
juanlncf <jmoncada@lazarusnaturals.com>
2026-01-08 11:38:18 -0800

    forgot value;

--- src/components/Bundle.tsx
+++ src/components/Bundle.tsx
@@ -59,7 +59,7 @@ export const Bundle = (customProductData) => {
                 <div 
                   className="cf-tiers-tier"
                   style={{
-                    '--cf-tier-progress':  (currentOrderValue - benefitTiers[index - 1 >= 0 ? index - 1 : 0]) / (tier.value - benefitTiers[index - 1 >= 0 ? index - 1 : 0]) * 100 + '%',
+                    '--cf-tier-progress':  (currentOrderValue - benefitTiers[index-- >= 0 ? index-- : 0].value) / (tier.value - benefitTiers[index-- >= 0 ? index-- : 0].value) * 100 + '%',
                   } as CSSProperties}
                 >
                   <div className="cf-tiers-tier-progress-text">
